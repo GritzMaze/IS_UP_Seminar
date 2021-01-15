6b55c83dfc1c0d641ae81d05bbd62db45c1f354e
GritzMaze <jivkovvesselin@gmail.com>
2021-01-15 14:14:37 +0200

    .

--- Kontrolno_2/task3.cpp
+++ Kontrolno_2/task3.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 
-bool check(double *arr, int size, int number, int times = 1, double sum = 1, int pos = 1)
+// Naive recursive function to check if triplet exists in array
+// with given sum
+bool tripletExists(double *arr, int size, double prod, int count)
 {
-  if(size == 0) return false;
-  if(sum == number) return true;
-  if(times == 3) return false;
-  double number1 = *arr;
-  double number2 = *arr + pos;
-  sum = number1*number2;
-  return check(arr + 1, size - 1, number, times + 1, sum, pos + 1);
+    // if triplet has desired sum, return true
+    if (count == 3 && prod == 1.0)
+        return true;
+
+    // return false if sum is not possible with current configuration
+    if (count == 3 || size == 0)
+        return false;
+
+    // recur with
+    // 1. including current element
+    // 2. excluding current element
+    return tripletExists(arr, size - 1, prod / arr[size - 1], count + 1) ||
+            tripletExists(arr, size - 1, prod, count);
 }
 
-bool isThere(double *arr, int size, int number)
+// Find Triplet with given sum in an array
+int main()
 {
-  if(size == 0) return false;
+    double arr[] = {5, 2, 1, 10, 3, 9, 1 };
+    double number = 6;
 
-  return check(arr, size, number) ? true : isThere(arr + 1, size - 1, number);
-}
+    int size = sizeof(arr) / sizeof(*arr);
 
-
-int main() {
-
-double arr[10] = {5, 2, 1, 10, 3, 9, 1};
-int size = 7;
-int number = 6;
-std::cout << isThere(arr, size, number);
-
-
-  return 0;
+    std::cout << std::boolalpha << tripletExists(arr, size, number, 0);
+    return 0;
 }
